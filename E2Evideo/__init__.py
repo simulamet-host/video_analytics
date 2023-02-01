ea5a20f14c7d2ec02c8dea2b8758820855dcf0fd
Faiga Alawad <faigaalawad@Faigas-MacBook-Air.local>
2023-02-01 18:27:13 +0100

    update init file

--- E2Evideo/__init__.py
+++ E2Evideo/__init__.py
@@ -1,2 +1,5 @@
-""" Empty initiate file. """
+""" E2E Video and Image Preprocessing for DL: Domain Independent Pipeline
+
+This project provides general modules for video and images preprocessing and feature extraction. Those are domain independent, but can be tailored towards specific problems by passing optional attributes to the different modules.
+"""
 __version__ = "0.0.0"
