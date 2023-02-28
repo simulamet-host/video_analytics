789958b590b2b10bae8752c5cd7df64146f3b718
Faiga Alawad <faiga.alawd91@gmail.com>
2023-02-28 15:11:43 +0000

    update video analytics

--- e2evideo/video_analytics.py
+++ e2evideo/video_analytics.py
@@ -6,15 +6,13 @@ This initial version is tailored specifically for the object detection dataset.
 import numpy as np
 import pandas as pd
 import os
+import random
 import matplotlib.pyplot as plt
-
 from tqdm import tqdm
 import cv2
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
-
 import itertools
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import *
 from tensorflow.keras.utils import to_categorical
@@ -86,31 +84,24 @@ x_train.shape, x_test.shape, np.array(y_train).shape, np.array(y_test).shape
 
 # Define the model
 model = Sequential()
-
 model.add(BatchNormalization(momentum=0.8, input_shape=(x_train.shape[1],x_train.shape[2], x_train.shape[3], 3)))
 model.add(ConvLSTM2D(filters = 16, kernel_size=(3,3), activation='LeakyReLU', data_format='channels_last', return_sequences=True, recurrent_dropout=0.2))
 model.add(MaxPooling3D(pool_size=(1,2,2), padding='same', data_format='channels_last'))
 model.add(TimeDistributed(Dropout(0.2)))
-
 model.add(ConvLSTM2D(filters = 16, kernel_size=(3,3), activation='LeakyReLU', data_format='channels_last', return_sequences=True, recurrent_dropout=0.2))
 model.add(BatchNormalization(momentum=0.8))
 model.add(MaxPooling3D(pool_size=(1,2,2), padding='same', data_format='channels_last'))
 model.add(TimeDistributed(Dropout(0.2)))
-
 model.add(ConvLSTM2D(filters = 16, kernel_size=(3,3), activation='LeakyReLU', data_format='channels_last', return_sequences=True, recurrent_dropout=0.2))
 model.add(BatchNormalization(momentum=0.8))
 model.add(MaxPooling3D(pool_size=(1,2,2), padding='same', data_format='channels_last'))
 model.add(TimeDistributed(Dropout(0.3)))
-
 model.add(ConvLSTM2D(filters = 16, kernel_size=(3,3), activation='LeakyReLU', data_format='channels_last', return_sequences=True, recurrent_dropout=0.2))
 model.add(BatchNormalization(momentum=0.8))
 model.add(MaxPooling3D(pool_size=(1,2,2), padding='same', data_format='channels_last'))
 model.add(TimeDistributed(Dropout(0.3)))
-
 model.add(Flatten())
-
 model.add(Dense(4096,activation="relu"))
-    
 model.add(Dense(10, activation='softmax'))
 model.summary()
 
@@ -125,8 +116,9 @@ history = model.fit(x_train, to_categorical(y_train), batch_size=32, epochs=50, 
 plt.figure(figsize=(13,5))
 plt.title("Accuracy vs Epochs")
 plt.plot(history.history['accuracy'], label='Train Accuracy')
-plt.plot(history.history['val_accuracy'], label='Test Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
 plt.legend(loc='best')
+plt.savefig('./results/accuracy_vs_epochs.png')
 plt.show()
 
 y_pred = model.predict(x_test)
@@ -136,48 +128,35 @@ for i in range(len(y_test)):
 
 print(accuracy_score(y_test, predicted_classes))
 
-import random
-
 # Create a Matplotlib figure
 plt.figure(figsize = (30, 30))
- 
 # Get Names of all classes in UCF101
-all_classes_names = label_data.labels.values
- 
+all_classes_names = label_data.labels.values 
 # Generate a random sample of images each time the cell runs
 random_range = random.sample(range(len(all_classes_names[0:10])), 5)
- 
 # Iterating through all the random samples
 for counter, random_index in enumerate(random_range, 1):
- 
     # Getting Class Name using Random Index
     selected_class_Name = all_classes_names[random_index]
- 
     # Getting a list of all the video files present in a Class Directory
     video_files_names_list = os.listdir(f'./data/UCF-101/{selected_class_Name}')
- 
     # Randomly selecting a video file
     selected_video_file_name = random.choice(video_files_names_list)
- 
     # Reading the Video File Using the Video Capture
     video_reader = cv2.VideoCapture(f'./data/UCF-101/{selected_class_Name}/{selected_video_file_name}')
-     
     # Reading The First Frame of the Video File
     _, bgr_frame = video_reader.read()
- 
     # Closing the VideoCapture object and releasing all resources. 
     video_reader.release()
- 
     # Converting the BGR Frame to RGB Frame 
     rgb_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2RGB)
- 
     # Adding The Class Name Text on top of the Video Frame.
     cv2.putText(rgb_frame, selected_class_Name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-     
     # Assigning the Frame to a specific position of a subplot
     plt.subplot(5, 4, counter)
     plt.imshow(rgb_frame)
     plt.axis('off')
+plt.savefig('./results/UCF101.png')
 
 #Confusion Matrix
 plt.figure(figsize=(25,25))
@@ -186,6 +165,7 @@ cm=confusion_matrix(y_test, predicted_classes)
 plt.imshow(cm)
 for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
     plt.text(j, i, cm[i, j], horizontalalignment="center")
+plt.savefig('./results/confusion_matrix.png')
 plt.show()
 
 if __name__ == '__main__':
