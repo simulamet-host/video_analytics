0a4ed1c41793d82dfb0190cc61c253c9f168f8c7
Faiga Alawad <faiga.alawd91@gmail.com>
2023-02-28 14:52:27 +0000

    add object detection module

--- e2evideo/video_analytics.py
+++ e2evideo/video_analytics.py
@@ -1,7 +1,192 @@
 """
-Domain_specific module. It contains functions that can be applied,
-to only specific dataset. This initial version is only applicable to the
-hand washing dataset.
+Object_detection module. It contains functions that are designed
+to perform object detection tasks on specific datasets.
+This initial version is tailored specifically for the object detection dataset.
 """
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+import cv2
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+import itertools
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import *
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+label_data = pd.read_csv("./data/UCF-101/ucfTrainTestlist/classInd.txt", sep=' ', header=None)
+label_data.columns=['index', 'labels']
+label_data = label_data.drop(['index'], axis=1)
+label_data.head()
+
+path=[]
+for label in label_data.labels.values:
+    path.append('./data/UCF-101/'+label+"/")
+
+#Function for Feature Extraction
+def feature_extraction(video_path):
+    width=60
+    height=60
+    sequence_length=10
+    frames_list=[]
+    #Read the Video
+    video_reader = cv2.VideoCapture(video_path)
+    #get the frame count
+    frame_count=int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
+    #Calculate the interval after which frames will be added to the list
+    skip_interval = max(int(frame_count/sequence_length), 1)
+    #iterate through video frames
+    for counter in range(sequence_length):
+        #Set the current frame postion of the video
+        video_reader.set(cv2.CAP_PROP_POS_FRAMES, counter * skip_interval)
+        #Read the current frame 
+        ret, frame = video_reader.read()
+        if not ret:
+            break;
+        #Resize the image
+        frame=cv2.resize(frame, (height, width))
+        frame = frame/255
+        #Append to the frame
+        frames_list.append(frame)
+    video_reader.release()
+    #Return the Frames List
+    return frames_list
+
+#Function for loading video files, Process and store in a data set
+def load_video(datasets):
+    global image
+    label_index=0
+    labels=[]
+    images=[]
+    #Iterate through each foler corresponding to category
+    for folder in datasets:
+        for file in tqdm(os.listdir(folder)):
+            #Get the path name for each video
+            video_path = os.path.join(folder, file)
+            #Extract the frames of the current video
+            frames_list = feature_extraction(video_path)
+            images.append(frames_list)
+            labels.append(label_index)
+        label_index+=1
+    return np.array(images, dtype='float16'), np.array(labels, dtype='int8')
+
+images, labels = load_video(path[:10])
+
+#Train Test Split
+x_train, x_test, y_train, y_test=train_test_split(images, labels, test_size=0.06, random_state=10)
+x_train.shape, x_test.shape, np.array(y_train).shape, np.array(y_test).shape
+
+# Define the model
+model = Sequential()
+
+model.add(BatchNormalization(momentum=0.8, input_shape=(x_train.shape[1],x_train.shape[2], x_train.shape[3], 3)))
+model.add(ConvLSTM2D(filters = 16, kernel_size=(3,3), activation='LeakyReLU', data_format='channels_last', return_sequences=True, recurrent_dropout=0.2))
+model.add(MaxPooling3D(pool_size=(1,2,2), padding='same', data_format='channels_last'))
+model.add(TimeDistributed(Dropout(0.2)))
+
+model.add(ConvLSTM2D(filters = 16, kernel_size=(3,3), activation='LeakyReLU', data_format='channels_last', return_sequences=True, recurrent_dropout=0.2))
+model.add(BatchNormalization(momentum=0.8))
+model.add(MaxPooling3D(pool_size=(1,2,2), padding='same', data_format='channels_last'))
+model.add(TimeDistributed(Dropout(0.2)))
+
+model.add(ConvLSTM2D(filters = 16, kernel_size=(3,3), activation='LeakyReLU', data_format='channels_last', return_sequences=True, recurrent_dropout=0.2))
+model.add(BatchNormalization(momentum=0.8))
+model.add(MaxPooling3D(pool_size=(1,2,2), padding='same', data_format='channels_last'))
+model.add(TimeDistributed(Dropout(0.3)))
+
+model.add(ConvLSTM2D(filters = 16, kernel_size=(3,3), activation='LeakyReLU', data_format='channels_last', return_sequences=True, recurrent_dropout=0.2))
+model.add(BatchNormalization(momentum=0.8))
+model.add(MaxPooling3D(pool_size=(1,2,2), padding='same', data_format='channels_last'))
+model.add(TimeDistributed(Dropout(0.3)))
+
+model.add(Flatten())
+
+model.add(Dense(4096,activation="relu"))
+    
+model.add(Dense(10, activation='softmax'))
+model.summary()
+
+#compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
+
+#Model training
+es = EarlyStopping(monitor='val_loss', patience=5, mode='min', restore_best_weights=True)
+history = model.fit(x_train, to_categorical(y_train), batch_size=32, epochs=50, validation_data=(x_test, to_categorical(y_test)), callbacks=[es])
+
+#Plot the graph to check training and testing accuracy over the period of time
+plt.figure(figsize=(13,5))
+plt.title("Accuracy vs Epochs")
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Test Accuracy')
+plt.legend(loc='best')
+plt.show()
+
+y_pred = model.predict(x_test)
+predicted_classes=[]
+for i in range(len(y_test)):
+    predicted_classes.append(np.argmax(y_pred[i]))
+
+print(accuracy_score(y_test, predicted_classes))
+
+import random
+
+# Create a Matplotlib figure
+plt.figure(figsize = (30, 30))
+ 
+# Get Names of all classes in UCF101
+all_classes_names = label_data.labels.values
+ 
+# Generate a random sample of images each time the cell runs
+random_range = random.sample(range(len(all_classes_names[0:10])), 5)
+ 
+# Iterating through all the random samples
+for counter, random_index in enumerate(random_range, 1):
+ 
+    # Getting Class Name using Random Index
+    selected_class_Name = all_classes_names[random_index]
+ 
+    # Getting a list of all the video files present in a Class Directory
+    video_files_names_list = os.listdir(f'./data/UCF-101/{selected_class_Name}')
+ 
+    # Randomly selecting a video file
+    selected_video_file_name = random.choice(video_files_names_list)
+ 
+    # Reading the Video File Using the Video Capture
+    video_reader = cv2.VideoCapture(f'./data/UCF-101/{selected_class_Name}/{selected_video_file_name}')
+     
+    # Reading The First Frame of the Video File
+    _, bgr_frame = video_reader.read()
+ 
+    # Closing the VideoCapture object and releasing all resources. 
+    video_reader.release()
+ 
+    # Converting the BGR Frame to RGB Frame 
+    rgb_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2RGB)
+ 
+    # Adding The Class Name Text on top of the Video Frame.
+    cv2.putText(rgb_frame, selected_class_Name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+     
+    # Assigning the Frame to a specific position of a subplot
+    plt.subplot(5, 4, counter)
+    plt.imshow(rgb_frame)
+    plt.axis('off')
+
+#Confusion Matrix
+plt.figure(figsize=(25,25))
+plt.title("Confusion matrix")
+cm=confusion_matrix(y_test, predicted_classes)
+plt.imshow(cm)
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(j, i, cm[i, j], horizontalalignment="center")
+plt.show()
+
 if __name__ == '__main__':
     print('currently empty file')
