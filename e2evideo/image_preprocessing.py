db5405a27de30bca3365baca825896156ed71a4f
Faiga Alawad <faiga.alawd91@gmail.com>
2023-03-05 18:41:19 +0100

    saved each video as a separate array

--- e2evideo/image_preprocessing.py
+++ e2evideo/image_preprocessing.py
@@ -26,8 +26,12 @@ def get_images(args_opt):
                         This array is of size = (num_images, height, width, no_channels)
     """
     img_folders = [x[0] for x in os.walk(args_opt.dir)]
+    # Each folder is its own video
+    print('Number of folders', len(img_folders[1:]))
+
     all_images = []
     for folder_name in img_folders:
+        video_file = []
         images_ = glob.glob(folder_name + "/"+ args_opt.img_format )
         for img_ in images_:
             img = cv2.imread(img_) # pylint: disable=E1101
@@ -35,7 +39,17 @@ def get_images(args_opt):
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # pylint: disable=E1101
             if args_opt.resize:
                 img = cv2.resize(img, (args_opt.img_width, args_opt.img_height)) # pylint: disable=E1101
-            all_images.append(img_as_float32(img))
+            video_file.append(img_as_float32(img))
+        print('Number of images in folder:', folder_name , ' is equal to ', len(video_file))
+        if len(video_file) !=0:
+            all_images.append(video_file)
+    # find the maximum length of the videos in the dataset
+    max_len = max([len(x) for x in all_images])
+    # pad the shorter videos with zeros at the end to make them all the same length
+    for i in range(len(all_images)):
+        if len(all_images[i]) < max_len:
+            all_images[i] = np.pad(all_images[i], ((0, max_len-len(all_images[i])), (0, 0), (0, 0)), 'constant')
+    # convert the list of videos to a numpy array
     all_images = np.array(all_images)
     # save all_images in a numpy array 
     np.save(args_opt.output, all_images)
@@ -53,5 +67,5 @@ if __name__ == '__main__':
     args_ = parser_.parse_args()
     
     _images = get_images(args_)
-  
+
     print('Images saved in array of arrays with size', str(_images.shape))
