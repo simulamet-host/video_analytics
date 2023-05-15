b2065662bf125832253607e8575a974482c5d500
Faiga Alawad <faiga.alawd91@gmail.com>
2023-05-15 23:30:04 +0200

    working on improving the frontend

--- e2evideo/e2e_app.py
+++ e2evideo/e2e_app.py
@@ -1,15 +1,27 @@
 import os
 import glob
+import random
 import subprocess
 import streamlit as st
+from PIL import Image
 
-st.title('e2e-Video')
+img = Image.open("logo.png")
+st.image(img, width=500)
 
-with st.sidebar:
-    st.write('St Session State' , st.session_state)
+st.title('e2eVideo Pipeline App')
 
-st.write(""" Please place your data files in a folder inside the 'data',
-located within the same directory as the running project.""")
+def colored_text(text, color):
+    st.markdown(
+        f"""
+        <div style="color: {color}; display: inline-block; padding: 5px;font-family: Times New Roman;">
+           <em> {text} </em>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+
+
+colored_text("Please place your data files in a folder inside the 'data' folder,  located within the same directory as the running project.", "Salmon")
 
 def get_subdirectories(path):
     subdirs = [dir_name for dir_name in os.listdir(path) if os.path.isdir(os.path.join(path, dir_name))]
@@ -22,11 +34,9 @@ def get_parent_directory(path):
 def folder_selector_ui(starting_path):
     if "subdirs" not in st.session_state:
         st.session_state.subdirs = get_subdirectories(starting_path)
-    if "selected_path" not in st.session_state:
         st.session_state.selected_path = starting_path
 
     st.write('Data directory: ', starting_path)
-    st.write('Select a folder containing videos inside the data directory.')
 
     selected_dir = st.selectbox('Select video folder', st.session_state.subdirs, key="video_folder_selectbox")
     column =  st.columns(3)  
@@ -46,7 +56,7 @@ def folder_selector_ui(starting_path):
                 st.session_state.subdirs = get_subdirectories(new_path)
             st.write('Selected folder:', st.session_state.selected_path)
         else:
-            st.write('No subdirectories found in the selected folder.')
+            colored_text("There are no subdirectories in this folder.", "gray")
             st.write('Selected folder:', st.session_state.selected_path)
 
     if column[2].button('Go up', key = "go_up_button"):
@@ -56,45 +66,74 @@ def folder_selector_ui(starting_path):
 
     return st.session_state.selected_path
     
+with st.expander("Video Preprocessing"):
+    # Set the starting path to the 'data' directory
+    current_directory = os.getcwd()
+    parent_directory = os.path.dirname(current_directory)
+    starting_path = os.path.join(parent_directory, 'data')
 
-# Set the starting path to the 'data' directory
-current_directory = os.getcwd()
-parent_directory = os.path.dirname(current_directory)
-starting_path = os.path.join(parent_directory, 'data')
+    # Check if the data directory exists
+    if not os.path.isdir(starting_path):
+        os.mkdir(starting_path)
+        # change the following line to a colored text
+        colored_text("New data directory is created!", "green")
+       
+    selected_folder = folder_selector_ui(starting_path)
 
-# Check if the data directory exists
-if not os.path.isdir(starting_path):
-    os.mkdir(starting_path)
-    st.write('Created data directory!')
-else:
-    st.write('Data directory exists!')
-
-selected_folder = folder_selector_ui(starting_path)
-
-cmd_list = ['python', 'video_preprocessing.py', '--videos_folder', selected_folder]
-with st.echo():
+    cmd_list = ['python', 'video_preprocessing.py', '--videos_folder', selected_folder]
+    
     output_folder = os.path.join(starting_path, 'frames')
-cmd_list.extend(['--output_folder', output_folder])
+    # check if output folder exists and create it if it doesn't
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+        colored_text("New frames directory is created!", "green")
+    colored_text("Frames will be saved in the following directory:", "gray")
+    st.write(output_folder)
 
-video_format = st.selectbox('Select video format', ('mp4', 'avi', 'mov', 'wmv', 'flv', 'mkv', 'webm', 'm4v', '3gp'),
-                            key='video_format_selectbox')
-image_format = st.selectbox('Select image format', ('jpg', 'png', 'bmp', 'tiff', 'gif', 'webp', 'ico', 'raw', 'eps', 'psd', 'svg'),
-                            key='image_format_selectbox')
-sampling_mode = st.selectbox('Select sampling mode', ('every_frame', 'per_second', 'fixed_frames'),
-                                key='sampling_mode_selectbox')
-cmd_list.extend(['--video_format', video_format, '--image_format', image_format, '--sampling_mode', sampling_mode])
+    cmd_list.extend(['--output_folder', output_folder])
 
-if sampling_mode == 'fixed_frames':
-    num_frames = st.number_input('Number of frames', min_value=1, max_value=1000, value=10, step=1)
-    cmd_list.extend(['--num_frames', str(num_frames)])
+    video_format = st.selectbox('Select video format', ('mp4', 'avi', 'mov', 'wmv', 'flv', 'mkv', 'webm', 'm4v', '3gp'),
+                                key='video_format_selectbox')
+    image_format = st.selectbox('Select image format', ('jpg', 'png', 'bmp', 'tiff', 'gif', 'webp', 'ico', 'raw', 'eps', 'psd', 'svg'),
+                                key='image_format_selectbox')
+    sampling_mode = st.selectbox('Select sampling mode', ('every_frame', 'per_second', 'fixed_frames'),
+                                    key='sampling_mode_selectbox')
+    cmd_list.extend(['--video_format', video_format, '--image_format', image_format, '--sampling_mode', sampling_mode])
 
-if st.button('Extract Frames', key='extract_frames_button'):
-    results = subprocess.run(cmd_list, capture_output=True)
-    if results.returncode == 0:
-        output = results.stdout.decode('utf-8')
-        st.write(f'Output: \n {output}')
-        st.success('Frames extracted successfully!')
-    else:
-        st.error('Error extracting frames!')
-        error = results.stderr.decode('utf-8')
-        st.write(f'Error: \n {error}')
+    if sampling_mode == 'fixed_frames':
+        num_frames = st.number_input('Number of frames', min_value=1, max_value=1000, value=10, step=1)
+        cmd_list.extend(['--num_frames', str(num_frames)])
+
+    if "extract_frames_clicked" not in st.session_state:
+        st.session_state.extract_frames_clicked = False
+
+    if st.button('Extract Frames', key='extract_frames_button'):
+        st.session_state.extract_frames_clicked = True
+        results = subprocess.run(cmd_list, capture_output=True)
+        if results.returncode == 0:
+            output = results.stdout.decode('utf-8')
+            st.text_area("Output", value=output, height=200)
+            st.success('Frames extracted successfully!')
+        else:
+            st.error('Error extracting frames!')
+            error = results.stderr.decode('utf-8')
+            st.write(f'Error: \n {error}')
+
+    if st.session_state.extract_frames_clicked:
+        if st.button('Show Example Frames', key='show_example_button'):
+                frames_subdir = get_subdirectories(output_folder)
+                random_frames_subdir = random.sample(frames_subdir, k=3)
+                for subdir in random_frames_subdir:
+                    frames = glob.glob(os.path.join(output_folder, subdir, "*." + image_format))
+                    frames.sort()
+                    selected_frames = frames[:3]
+                    st.image(selected_frames, width=500)
+
+with st.expander("Image Preprocessing"):
+    st.write('Select a folder containing images inside the data directory.')
+
+with st.expander("Feature Extraction"):
+    st.write('Select a folder containing images inside the data directory.')
+
+with st.expander("Classification"):
+    st.write('Select a folder containing images inside the data directory.')
