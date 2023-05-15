20430ed3957fe746ddfc2096bf524257db9fcc19
Faiga Alawad <faiga.alawd91@gmail.com>
2023-05-15 23:48:02 +0200

    Fix pylint errors

--- e2evideo/e2e_app.py
+++ e2evideo/e2e_app.py
@@ -1,3 +1,6 @@
+"""
+This module contains the streamlit app for the e2eVideo pipeline.
+"""
 import os
 import glob
 import random
@@ -11,9 +14,22 @@ st.image(img, width=500)
 st.title('e2eVideo Pipeline App')
 
 def colored_text(text, color):
+    """
+    This function displays colored text in the streamlit app.
+    Parameters
+    ----------
+    text : str
+        The text to be displayed.
+    color : str
+        The color of the text.
+    Returns
+    -------
+    None
+    """
     st.markdown(
         f"""
-        <div style="color: {color}; display: inline-block; padding: 5px;font-family: Times New Roman;">
+        <div style="color: {color}; display: inline-block;
+                padding: 5px;font-family: Times New Roman;">
            <em> {text} </em>
         </div>
         """,
@@ -21,25 +37,61 @@ def colored_text(text, color):
     )
 
 
-colored_text("Please place your data files in a folder inside the 'data' folder,  located within the same directory as the running project.", "Salmon")
+colored_text("Please place your data files in a folder inside the 'data' folder,\
+              located within the same directory as the running project.", "Salmon")
 
 def get_subdirectories(path):
-    subdirs = [dir_name for dir_name in os.listdir(path) if os.path.isdir(os.path.join(path, dir_name))]
+    """
+    This function returns a list of subdirectories in a given path.
+    Parameters
+    ----------
+    path : str
+        The path to the directory.
+    Returns
+    -------
+    subdirs : list
+        A list of subdirectories in the given path.
+    """
+    subdirs = [dir_name for dir_name in os.listdir(path)
+               if os.path.isdir(os.path.join(path, dir_name))]
     return subdirs
 
 def get_parent_directory(path):
+    """
+    This function returns the parent directory of a given path.
+    Parameters
+    ----------
+    path : str
+        The path to the directory.
+    Returns
+    -------
+    parent_dir : list
+        A list containing the parent directory of the given path.
+    """
     parent_dir = [os.path.dirname(path)]
     return parent_dir
 
-def folder_selector_ui(starting_path):
+def folder_selector_ui(input_path):
+    """
+    This function displays a folder selector user interface.
+    Parameters
+    ----------
+    input_path : str
+        The path to the starting directory.
+    Returns
+    -------
+    selected_path : str
+        The path to the selected directory.
+    """
     if "subdirs" not in st.session_state:
-        st.session_state.subdirs = get_subdirectories(starting_path)
-        st.session_state.selected_path = starting_path
+        st.session_state.subdirs = get_subdirectories(input_path)
+        st.session_state.selected_path = input_path
 
-    st.write('Data directory: ', starting_path)
+    st.write('Data directory: ', input_path)
 
-    selected_dir = st.selectbox('Select video folder', st.session_state.subdirs, key="video_folder_selectbox")
-    column =  st.columns(3)  
+    selected_dir = st.selectbox('Select video folder', st.session_state.subdirs,
+                                key="video_folder_selectbox")
+    column =  st.columns(3)
 
     if column[0].button('Select folder', key="select_folder_button"):
         new_path = os.path.join(st.session_state.selected_path, selected_dir)
@@ -63,9 +115,8 @@ def folder_selector_ui(starting_path):
         parent_dir = get_parent_directory(st.session_state.selected_path)
         st.session_state.selected_path = parent_dir[0]
         st.session_state.subdirs = get_subdirectories(parent_dir[0])
-
     return st.session_state.selected_path
-    
+
 with st.expander("Video Preprocessing"):
     # Set the starting path to the 'data' directory
     current_directory = os.getcwd()
@@ -77,11 +128,11 @@ with st.expander("Video Preprocessing"):
         os.mkdir(starting_path)
         # change the following line to a colored text
         colored_text("New data directory is created!", "green")
-       
+
     selected_folder = folder_selector_ui(starting_path)
 
     cmd_list = ['python', 'video_preprocessing.py', '--videos_folder', selected_folder]
-    
+
     output_folder = os.path.join(starting_path, 'frames')
     # check if output folder exists and create it if it doesn't
     if not os.path.isdir(output_folder):
@@ -92,16 +143,21 @@ with st.expander("Video Preprocessing"):
 
     cmd_list.extend(['--output_folder', output_folder])
 
-    video_format = st.selectbox('Select video format', ('mp4', 'avi', 'mov', 'wmv', 'flv', 'mkv', 'webm', 'm4v', '3gp'),
+    video_format = st.selectbox('Select video format', ('mp4', 'avi', 'mov', 'wmv', 'flv',
+                                                        'mkv', 'webm', 'm4v', '3gp'),
                                 key='video_format_selectbox')
-    image_format = st.selectbox('Select image format', ('jpg', 'png', 'bmp', 'tiff', 'gif', 'webp', 'ico', 'raw', 'eps', 'psd', 'svg'),
+    image_format = st.selectbox('Select image format', ('jpg', 'png', 'bmp', 'tiff',
+                                'gif', 'webp', 'ico', 'raw', 'eps', 'psd', 'svg'),
                                 key='image_format_selectbox')
-    sampling_mode = st.selectbox('Select sampling mode', ('every_frame', 'per_second', 'fixed_frames'),
-                                    key='sampling_mode_selectbox')
-    cmd_list.extend(['--video_format', video_format, '--image_format', image_format, '--sampling_mode', sampling_mode])
+    sampling_mode = st.selectbox('Select sampling mode', ('every_frame', 'per_second',
+                                                          'fixed_frames'), 
+                                                          key='sampling_mode_selectbox')
+    cmd_list.extend(['--video_format', video_format, '--image_format', image_format,
+                     '--sampling_mode', sampling_mode])
 
     if sampling_mode == 'fixed_frames':
-        num_frames = st.number_input('Number of frames', min_value=1, max_value=1000, value=10, step=1)
+        num_frames = st.number_input('Number of frames', min_value=1, max_value=1000,
+                                     value=10, step=1)
         cmd_list.extend(['--num_frames', str(num_frames)])
 
     if "extract_frames_clicked" not in st.session_state:
@@ -109,7 +165,7 @@ with st.expander("Video Preprocessing"):
 
     if st.button('Extract Frames', key='extract_frames_button'):
         st.session_state.extract_frames_clicked = True
-        results = subprocess.run(cmd_list, capture_output=True)
+        results = subprocess.run(cmd_list, capture_output=True, check=False)
         if results.returncode == 0:
             output = results.stdout.decode('utf-8')
             st.text_area("Output", value=output, height=200)
@@ -121,13 +177,13 @@ with st.expander("Video Preprocessing"):
 
     if st.session_state.extract_frames_clicked:
         if st.button('Show Example Frames', key='show_example_button'):
-                frames_subdir = get_subdirectories(output_folder)
-                random_frames_subdir = random.sample(frames_subdir, k=3)
-                for subdir in random_frames_subdir:
-                    frames = glob.glob(os.path.join(output_folder, subdir, "*." + image_format))
-                    frames.sort()
-                    selected_frames = frames[:3]
-                    st.image(selected_frames, width=500)
+            frames_subdir = get_subdirectories(output_folder)
+            random_frames_subdir = random.sample(frames_subdir, k=3)
+            for subdir in random_frames_subdir:
+                frames = glob.glob(os.path.join(output_folder, subdir, "*." + image_format))
+                frames.sort()
+                selected_frames = frames[:3]
+                st.image(selected_frames, width=500)
 
 with st.expander("Image Preprocessing"):
     st.write('Select a folder containing images inside the data directory.')
