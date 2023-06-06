4c9696e1e0013f78c3423aa8b94583de37dd8356
Faiga Alawad <faiga.alawd91@gmail.com>
2023-06-06 15:58:01 +0200

    fix pylint errors

--- e2evideo/video_preprocessing.py
+++ e2evideo/video_preprocessing.py
@@ -8,88 +8,136 @@ Main functionality:
 """
 # import packages needed
 import argparse
+from dataclasses import dataclass
 import os
 import glob
 import math
 import cv2
-import numpy as np
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+OUTPUT_PATH = '../data/ucf_sports_actions/frames'
 
-def create_folder(dir_):
-    """
-    create a folder at dir if it does not exist already.
-    Input:
-        - dir: the folder directory
-    """
-    try:
-        os.makedirs(dir_)
-    except OSError:
-        pass
+@dataclass
+class VideoConfig:
+    """Configuration class for video pre-processing."""
+    videos_folder: str
+    video_format: str = 'mp4'
+    image_format: str = 'jpg'
+    sampling_mode: str = 'every_frame'
+    num_frames: int = 10
+    output_folder: str = OUTPUT_PATH
 
-def main(options):
-    """
-    Main function will run when the python file is called from terminal.
-    """
-    assert os.path.isdir(options.videos_folder), 'The given videos_folder does not exist'
-    create_folder(options.output_folder)
-    search_path = os.path.join(options.videos_folder, '**', '*' + options.video_format)
-    video_files = glob.glob(search_path, recursive=True)
-    #video_files = glob.glob(options.videos_folder + "/**/*." + options.video_format,
-            #                recursive=True)
-    assert len(video_files) != 0 , 'The given videos folder does not contain any vidoes'
+@dataclass
+class FrameData:
+    """Data class for frame data."""
+    frame: any = None
+    frames: list = None
+    frame_indices: list = None
+    count: int  = 0
+    frames_folder: str  = None
+    frame_rate: int = 0
 
-    for video_file in video_files:
-        print('\n\n')
-        video_format_len = len(options.video_format) + 1
-        images_sub_folder = video_file.split('/')[-1][:-video_format_len]
-        frames_folder = os.path.join(options.output_folder, images_sub_folder)
-        create_folder(frames_folder)
-        # capture the video from the video file
-        cap = cv2.VideoCapture(video_file) # pylint: disable=E1101
-        frame_rate = cap.get(cv2.CAP_PROP_FPS) # pylint: disable=E1101
-        total_num_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT) # pylint: disable=E1101
-        video_length = total_num_frames / frame_rate
-        n_duration = video_length / options.num_frames
-        if options.num_frames > total_num_frames:
-            print(f"Warning: the number of frames is larger than the total number of frames \
-                  in the video {video_file}")
-            frame_indices = [i for i in range(int(total_num_frames))]
+class VideoPreprocessor:
+    """Video pre-processing class that extract frames from videos."""
+    def __init__(self, config: VideoConfig):
+        """initialize the class."""
+        self.config = config
+
+    def create_folder(self, dir_):
+        """
+        create a folder at dir if it does not exist already.
+        Input:
+            - dir: the folder directory
+        """
+        try:
+            os.makedirs(dir_)
+        except OSError:
+            pass
+
+    def get_video_files(self):
+        """Get all video files from the given videos folder."""
+        assert os.path.isdir(self.config.videos_folder), 'The given videos_folder does not exist'
+        self.create_folder(self.config.output_folder)
+        search_path = os.path.join(self.config.videos_folder, '**', '*' + self.config.video_format)
+        video_files = glob.glob(search_path, recursive=True)
+        assert len(video_files) != 0 , 'The given videos folder does not contain any vidoes'
+        return video_files
+
+    def calculate_frame_indices(self, frame_rate, total_num_frames, video_file):
+        """Calculate the frame indices for the fixed number of frames mode."""
+        if self.config.num_frames > total_num_frames:
+            print(f"Warning: the number of frames is larger than the total number of frames"
+                    f"in the video {video_file}")
+            frame_indices = list(range(int(total_num_frames)))
         else:
-            frame_indices = [round(frame_rate * i) for i in range(options.num_frames)]
-        count = 0
-        frames = []
-        while cap.isOpened():
-            frame_id = cap.get(cv2.CAP_PROP_POS_FRAMES) # pylint: disable=E1101
-            ret, frame = cap.read()
-            if not ret:
-                break
-            file_name = f"frame{count}." + options.image_format            
-            if options.sampling_mode == 'every_frame':
-                cv2.imwrite(frames_folder + '/' + file_name, frame) # pylint: disable=E1101
-                count += 1
-            elif options.sampling_mode == 'per_second':
-                if frame_id % math.floor(frame_rate) == 0:
-                    cv2.imwrite(frames_folder + '/'  + file_name, frame) # pylint: disable=E1101
-                    count += 1
-            elif options.sampling_mode == 'fixed_frames':
-                if frame_id in frame_indices:
-                    cv2.imwrite(frames_folder + '/' + file_name, frame)
-                    count += 1
-                    frames.append(frame)
-            # interpolate missing frames if count is less than the number of frames
-            if frame_id == total_num_frames - 1 and options.sampling_mode == 'fixed_frames' and len(frames) < options.num_frames:
-                print(f"Warning: the number of frames extracted from {video_file} is less than the number of frames specified")
-                print(f"Interpolating missing frames...")
-                while len(frames) < options.num_frames:
-                    alpha = float(len(frames)) / len(frame_indices)
-                    interpolated_frame = cv2.addWeighted(frame, 1 - alpha, frames[-1], alpha, 0)
-                    frames.append(interpolated_frame)
-                    file_name = f"frame{count}." + options.image_format
-                    cv2.imwrite(frames_folder + '/' + file_name, interpolated_frame)
-                    count += 1
-        cap.release()
-        print(f"Done! {count} images of format JPG is saved in {frames_folder}" )
+            frame_indices = [round(frame_rate * i) for i in range(self.config.num_frames)]
+        return frame_indices
+
+    def interpolate_missing_frames(self, frame_data: FrameData):
+        """Interpolate missing frames if the number of frames is less than the number of frames."""        
+        while len(frame_data.frames) < self.config.num_frames:
+            alpha = float(len(frame_data.frames)) / len(frame_data.frame_indices)
+            # pylint: disable=E1101
+            interpolated_frame = cv2.addWeighted(frame_data.frame, 1 - alpha, frame_data.frames[-1],
+                                                 alpha, 0)
+            frame_data.frames.append(interpolated_frame)
+            file_name = f"frame{frame_data.count}." + self.config.image_format
+            cv2.imwrite(frame_data.frames_folder + '/' + file_name, interpolated_frame)
+            frame_data.count += 1
+        return frame_data.count
+
+    def process_video(self):
+        """
+        Main function will run when the python file is called from terminal.
+        """
+        video_files = self.get_video_files()
+        for video_file in video_files:
+            print('\n\n')
+            frame_data = FrameData()
+            video_format_len = len(self.config.video_format) + 1
+            images_sub_folder = video_file.split('/')[-1][:-video_format_len]
+            frame_data.frames_folder = os.path.join(self.config.output_folder, images_sub_folder)
+            self.create_folder(frame_data.frames_folder)
+            # capture the video from the video file
+            cap = cv2.VideoCapture(video_file) # pylint: disable=E1101
+            frame_data.frame_rate = cap.get(cv2.CAP_PROP_FPS) # pylint: disable=E1101
+            total_num_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT) # pylint: disable=E1101
+            frame_data.frame_indices = self.calculate_frame_indices(frame_data.frame_rate,
+                                                total_num_frames, video_file)
+            frame_data.count = 0
+            frame_data.frames = []
+            cond1 = self.config.sampling_mode == 'fixed_frames'
+            while cap.isOpened():
+                frame_id = cap.get(cv2.CAP_PROP_POS_FRAMES) # pylint: disable=E1101
+                ret, frame_data.frame = cap.read()
+                if not ret:
+                    break
+                file_name = f"frame{frame_data.count}." + self.config.image_format
+                if self.config.sampling_mode == 'every_frame':
+                    cv2.imwrite(frame_data.frames_folder + '/' + file_name, frame_data.frame) # pylint: disable=E1101
+                    frame_data.count += 1
+                elif self.config.sampling_mode == 'per_second':
+                    if frame_id % math.floor(frame_data.frame_rate) == 0:
+                        cv2.imwrite(frame_data.frames_folder + '/'  + file_name, frame_data.frame) # pylint: disable=E1101
+                        frame_data.count += 1
+                elif cond1:
+                    if frame_id in frame_data.frame_indices:
+                        # pylint: disable=E1101
+                        cv2.imwrite(frame_data.frames_folder + '/' + file_name, frame_data.frame)
+                        frame_data.count += 1
+                        frame_data.frames.append(frame_data.frame)
+                # interpolate missing frames if count is less than the number of frames
+                cond2 = frame_id == total_num_frames - 1
+                cond3 = len(frame_data.frames) < self.config.num_frames
+                if cond1 and cond2 and cond3:
+                    print((f"Warning: the number of frames extracted from {video_file} is"
+                            "less than the number of frames specified"))
+                    print("Interpolating missing frames...")
+                    #frame_data = FrameData(frame, frames, frame_indices, count, frames_folder)
+                    frame_data.count = self.interpolate_missing_frames(frame_data)
+            cap.release()
+            print(f"Done! {frame_data.count} images of format JPG is"
+                  f"saved in {frame_data.frames_folder}" )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -97,8 +145,12 @@ if __name__ == '__main__':
     parser.add_argument('--video_format', default='mp4', help='choose the video format to read.')
     parser.add_argument('--image_format', default='jpg',
                         help='choose the format for the output images.')
-    parser.add_argument('--sampling_mode', default='every_frame', choices= ['fixed_frames', 'every_frame', 'per_second'])
+    parser.add_argument('--sampling_mode', default='every_frame',
+                        choices= ['fixed_frames', 'every_frame', 'per_second'])
     parser.add_argument('--num_frames', default=10, type=int)
-    parser.add_argument('--output_folder', default='../data/ucf_sports_actions/frames')
+    parser.add_argument('--output_folder', default=OUTPUT_PATH)
     opts = parser.parse_args()
-    main(opts)
+    videos_config = VideoConfig(opts.videos_folder, opts.video_format, opts.image_format,
+                            opts.sampling_mode, opts.num_frames, opts.output_folder)
+    processor = VideoPreprocessor(videos_config)
+    processor.process_video()
