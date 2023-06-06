ccf27d2a53148f87a77b7ed0a2f97f8ac95a576c
Faiga Alawad <faiga.alawd91@gmail.com>
2023-06-06 16:09:54 +0200

    fix pylint errors

--- e2evideo/feature_extractor.py
+++ e2evideo/feature_extractor.py
@@ -15,6 +15,7 @@ import conv_autoencoder
 import load_ucf101
 import our_utils
 import plot_results
+from load_ucf11 import get_data
 
 device = our_utils.get_device()
 
@@ -80,17 +81,19 @@ def main(args_):
         #training_data = np.concatenate(train_gen[:, 0])
         #test_data = np.concatenate(test_gen[:, 0])
         #visual_data = test_data
-
+    else:
+        train_dataset, test_dataset = get_data(args_.labels_file, args_.images_array)
     if args_.mode == 'train':
         #  training model
         model = conv_autoencoder.ConvolutionalAutoencoder(conv_autoencoder.Autoencoder(
                 conv_autoencoder.Encoder(), conv_autoencoder.Decoder()))
-        training_args = {'loss_function': nn.BCELoss(), 'epochs': 50 , 'batch_size': 5,
-                    'training_set': x_train, 'test_set': x_test, 'visual_set': x_test}
+        training_args = {'loss_function': nn.BCELoss(), 'epochs': args_.epochs,
+                         'batch_size': args_.batch_size, 'training_set': train_dataset,
+                         'test_set': test_dataset, 'visual_set': test_dataset}
         log_dict = model.train(training_args)
         print(log_dict)
     else:
-        visual_data = DataLoader(x_test)
+        visual_data = DataLoader(test_dataset)
         #  loading model
         saved_model = torch.load('./checkpoints/model-11.pt')
         plot_results.plot_cae_training(visual_data, saved_model)
@@ -102,8 +105,11 @@ if __name__ == '__main__':
     parser.add_argument('--dataset_name', default='action_recognition')
     parser.add_argument('--images_array', type=str, default='./results/ucf10.npz')
     parser.add_argument('--data_folder', type=str, default='../data/images_ucf10/')
+    parser.add_argument('--labels_file', type=str, default=None)
     parser.add_argument('--no_classes', type=int, default=10)
     parser.add_argument('--mode', type=str, default='train')
+    parser.add_argument('--batch_size', type=int, default=50)
+    parser.add_argument('--epochs', type=int, default=50)
     args_input = parser.parse_args()
 
     device = our_utils.get_device()
