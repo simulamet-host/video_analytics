f0b46eb765b356711813026cce698610ff8a9040
Faiga Alawad <faiga@srl-login1.cm.cluster>
2023-09-07 13:28:38 +0200

    add a slrum job to run the dinov2

---
+++ e2evideo/jobs/dinov2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name="dinov2"
+#SBATCH --partition=habanaq
+#SBATCH --time=0-00:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --output="./logs/%j-%x-stdout.txt"
+#SBATCH --error="./logs/%j-%x-stderr.txt"
+
+echo "This is Faiga's process running on $(hostname)"
+eval "$(conda shell.bash hook)"
+conda activate video
+cd ..
+srun python dinov2.py
+echo 'Done!'
