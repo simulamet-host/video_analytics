3257edbee364f5a020295995eae05cbdba70423a
Faiga Alawad <faiga.alawd91@gmail.com>
2023-09-15 11:52:09 +0200

    fix pylint issues

--- e2evideo/feature_extractor.py
+++ e2evideo/feature_extractor.py
@@ -1,15 +1,22 @@
+"""
+This module contains the code for extracting features from images using
+DINOv2 and img2vec.
+"""
+# pylint: disable=no-member
+# pylint: disable=protected-access
 import os
 import argparse
+import logging
 import pandas as pd
 import torch
-import torchvision.models as models
-import torchvision.transforms as transforms
+from torchvision import models
+from torchvision import transforms
 from PIL import Image
 from tqdm import tqdm
 import fastdup
 from sklearn.manifold import TSNE
 import plotly.express as px
-import logging
+
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -19,6 +26,8 @@ logger.debug("Loaded dependencies successful!")
 
 
 class FeatureExtractor:
+    """feature extractor class"""
+
     def __init__(self, input_path, output_path):
         self.input_path = input_path
         self.output_path = output_path
@@ -29,23 +38,25 @@ class FeatureExtractor:
         self.to_tensor = transforms.ToTensor()
 
     def get_vector(self, image_name, layer, model):
+        """get vector from image"""
         img = Image.open(image_name)
         t_img = self.normalize(self.to_tensor(self.scaler(img))).unsqueeze(0)
         my_embedding = torch.zeros(512)
 
-        def copy_data(m, i, o):
-            my_embedding.copy_(o.data.squeeze())
+        def copy_data(output):
+            my_embedding.copy_(output.data.squeeze())
 
-        h = layer.register_forward_hook(copy_data)
+        temp_out = layer.register_forward_hook(copy_data)
         model(t_img)
-        h.remove()
+        temp_out.remove()
         return my_embedding
 
     def extract_img_vector(self):
+        """extract image vector using resnet18"""
         model = models.resnet18(pretrained=True)
         layer = model._modules.get("avgpool")
         model.eval()
-        logger.debug(f"{model} loaded successful!")
+        logger.debug("%s loaded successful!", model)
 
         vec_list = []
 
@@ -55,21 +66,21 @@ class FeatureExtractor:
             img = Image.open(img_path)
             if len(img.split()) != 3:
                 continue
-            else:
-                vec = self.get_vector(img_path, layer, model)
-                vec = vec.numpy().tolist()
-                vec_dict = {"Image_Name": img_name}
-                for i, v in enumerate(vec):
-                    vec_dict[f"Vector_{i}"] = v
-                vec_list.append(vec_dict)
+            vec = self.get_vector(img_path, layer, model)
+            vec = vec.numpy().tolist()
+            vec_dict = {"Image_Name": img_name}
+            for i, vector_ in enumerate(vec):
+                vec_dict[f"Vector_{i}"] = vector_
+            vec_list.append(vec_dict)
 
         vec_df = pd.DataFrame(vec_list)
         vec_df.to_csv(f"{self.output_path}/vec_df.csv", index=False)
         return vec_df
 
     def extract_dinov2_features(self):
-        fd = fastdup.create(input_dir=self.input_path)
-        fd.run(model_path="dinov2s", cc_threshold=0.8)
+        """extract features using DINOv2"""
+        fd_model = fastdup.create(input_dir=self.input_path)
+        fd_model.run(model_path="dinov2s", cc_threshold=0.8)
 
         filenames, feature_vec = fastdup.load_binary_feature(
             f"{self.output_path}/atrain_features.dat", d=384
@@ -85,7 +96,7 @@ def plot_tsne_3d(feature_vec, connected_components_df, filenames, output_path):
     tsne = TSNE(n_components=3, verbose=1, perplexity=40, n_iter=300)
     tsne_result = tsne.fit_transform(feature_vec)
     component_id = connected_components_df["component_id"].to_numpy()
-    df = pd.DataFrame(
+    results = pd.DataFrame(
         {
             "tsne_1": tsne_result[:, 0],
             "tsne_2": tsne_result[:, 1],
@@ -96,7 +107,7 @@ def plot_tsne_3d(feature_vec, connected_components_df, filenames, output_path):
     )
 
     fig = px.scatter_3d(
-        df,
+        results,
         x="tsne_1",
         y="tsne_2",
         z="tsne_3",
@@ -105,7 +116,7 @@ def plot_tsne_3d(feature_vec, connected_components_df, filenames, output_path):
         hover_data=["component", "filename"],
     )
 
-    fig.write_html("work_dir/embeddings_tsne2.html")
+    fig.write_html(output_path)
 
 
 if __name__ == "__main__":
@@ -120,15 +131,15 @@ if __name__ == "__main__":
     fe = FeatureExtractor(args.input_path, args.output_path)
 
     if args.feature_extractor == "dinov2":
-        filenames, feature_vec = fe.extract_dinov2_features()
+        filenames_, feature_vec_ = fe.extract_dinov2_features()
 
-        connected_components_df = pd.read_csv(
+        connected_components_df_ = pd.read_csv(
             os.path.join("work_dir", "connected_components.csv")
         )
         plot_tsne_3d(
-            feature_vec,
-            connected_components_df,
-            filenames,
+            feature_vec_,
+            connected_components_df_,
+            filenames_,
             "./work_dir/embeddings_dinvo2.html",
         )
     elif args.feature_extractor == "img2vec":
